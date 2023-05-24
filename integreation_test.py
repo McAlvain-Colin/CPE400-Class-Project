80ed51f678e59de8b5e91219eca49d72c3cf6ca1
McAlvain-Colin <cmcalvain@nevada.unr.edu>
2023-05-23 19:11:42 -0700

    added potential class files, and provided alway true tests in test files to test docker connection

--- integreation_test.py
+++ integreation_test.py
@@ -3,6 +3,9 @@ import unittest
 class TCPIntegrationTests(unittest.TestCase):
     def setUp(self):
         pass
+    def myTest(self):
+        a = 1
+        self.assertEqual(a, 1)
 
 if __name__ == "__main__":
     unittest.main()
